8ea5958a33871f4fb7464806b7dd0b9629eaab7d
TakashiAihara <user.email=47384567+TakashiAihara@users.noreply.github.com>
2023-03-15 15:49:33 +0000

    complete jan site seeds #15

---
+++ packages/db/prisma/seeds/janOriginSite.ts
@@ -0,0 +1,13 @@
+// HACK: define types
+const seed = [
+  {
+    id: "001",
+    title: "じゃん検索: JANコード検索＆バーコードリーダー",
+  },
+  {
+    id: "002",
+    title: "JANコード 商品検索",
+  },
+];
+
+export default seed;
