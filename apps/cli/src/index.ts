728f373e3979fcfa45600125070fcfc384c20c4c
TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
2023-06-25 14:39:23 +0000

    Close #70 --- Squashed commit of the following:
    commit 7de674d740722fd8b91aba85150989d245be8f38
    Author: TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
    Date:   Sun Jun 25 14:39:15 2023 +0000
    
        update pnpm-lock
    
    commit 3c912f33a6ca6cb3a5d157c3d8d9b48f1f20e02a
    Author: TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
    Date:   Sun Jun 25 14:38:26 2023 +0000
    
        add apps/cli basic

---
+++ apps/cli/src/index.ts
@@ -0,0 +1,53 @@
+import type { AppRouter } from "@acme/api";
+import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import { Command, Option } from "commander";
+import superjson from "superjson";
+
+type OptionType = { origin: string; quantity: number };
+
+const createTRPCClient = (origin: string) => {
+  return createTRPCProxyClient<AppRouter>({
+    transformer: superjson, // Required option due to inherited type
+    links: [
+      httpBatchLink({
+        url: `${origin}/api/trpc`,
+      }),
+    ],
+  });
+};
+
+const originOption = new Option("-o, --origin <origin>", "origin").default(
+  "http://localhost:3000"
+);
+const quantityOption = new Option("-q, --quantity <quantity>", "quantity")
+  .default(1)
+  .argParser<number>(parseInt);
+
+const program = new Command();
+const queueCommand = program.command("queue");
+
+queueCommand
+  .description("Queue JAN code")
+  .command("add")
+  .argument("<jan>", "jan")
+  .addOption(quantityOption)
+  .addOption(originOption)
+  .action(async (jan: string, { origin, quantity }: OptionType) => {
+    const result = await createTRPCClient(origin).queue.createOrAdd.mutate({
+      code: jan,
+      quantity,
+    });
+    console.log("Added successfully.");
+    console.log(result);
+  });
+
+queueCommand
+  .description("Get queued JAN codes")
+  .command("get")
+  .addOption(originOption)
+  .action(async ({ origin }: OptionType) => {
+    const result = await createTRPCClient(origin).queue.all.query();
+    console.log(result);
+  });
+
+program.parse(process.argv);
